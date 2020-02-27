b866be6914e52ec3a8ec3bbd82d8407ebd61cd8d
Alexis Chen <anc009@its-gl1w-mac05.ucsd.edu>
2020-02-26 21:07:02 -0800

    first commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Alexis Chen on 2/26/20.
+//  Copyright © 2020 Alexis Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
